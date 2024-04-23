b6cce3f4161032e28518b6967a2f7243939d210b
NathanMachrouh <machrouh.nathan@gmail.com>
2024-04-23 06:48:28 +0200

    update projet

--- src/components/Projets.jsx
+++ src/components/Projets.jsx
@@ -6,7 +6,7 @@ const Projets = ({ projet }) => {
                 <li>
                     <p><span>Titre:</span> {projet.titre} </p>
                     <p><span>Description:</span> {projet.description} </p>
-                    <p><span>Competence:</span> {projet.competence} </p>
+                    <p><span>Compétences:</span> {projet.competence} </p>
                     <p className="techno"><span>Techno:</span> {projet.techno} </p>
                     <p><span>Lien du répo:</span> <a href={projet.lien} rel="noreferrer" target="_blank">ICI</a></p>
                     <p><span>Démo:</span> <a href={projet.demo} rel="noreferrer" target="_blank">ICI</a></p>
